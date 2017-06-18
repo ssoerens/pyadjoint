96dea070add344ac1272ecf13e235848ff9d3a99
chukren <youyir@princeton.edu>
2017-06-18 17:22:17 -0400

    add two more examples

--- example/run_example.bash
+++ example/run_example.bash
@@ -2,3 +2,11 @@
 parfile="params/multitaper.adjoint.60_100.config.yml"
 python example_multitaper.py \
     -p $parfile
+
+parfile="params/cc_traveltime.adjoint.60_100.config.yml"
+python example_multitaper.py \
+    -p $parfile
+
+parfile="params/waveform.adjoint.60_100.config.yml"
+python example_multitaper.py \
+    -p $parfile
