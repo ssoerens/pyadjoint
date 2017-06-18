2626e7703f882ab7ba039df76c8162e322cfc194
chukren <youyir@princeton.edu>
2017-06-18 17:25:34 -0400

    change example script name

--- example/run_example.bash
+++ example/run_example.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 parfile="params/multitaper.adjoint.60_100.config.yml"
-python example_multitaper.py \
+python example_adjoint.py \
     -p $parfile
 
 parfile="params/cc_traveltime.adjoint.60_100.config.yml"
-python example_multitaper.py \
+python example_adjoint.py \
     -p $parfile
 
 parfile="params/waveform.adjoint.60_100.config.yml"
-python example_multitaper.py \
+python example_adjoint.py \
     -p $parfile
